f5372bbac6498ac9c743ebb30d543407ac41693e
eberthrosales <eberthrosalescastillo@gmail.com>
2024-04-29 11:51:48 -0500

    correcciones

--- ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-catalogo-service", path = "/producto")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallBackProducto")
     ResponseEntity<ProductoDto> productoBuscarPorId(@PathVariable(required = true) Integer id) ;
+    default ResponseEntity<ProductoDto>fallBackProducto(Integer id, Exception e) {
+
+        return  ResponseEntity.ok(new ProductoDto());
+    }
 
 }
--- ms-pedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ClienteDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-cliente-service", path = "/cliente")
 public interface ClienteFeign {
     @GetMapping("/{id}")
-    ResponseEntity<ClienteDto> buscarPorId(@PathVariable(required = true) Integer id) ;
+    @CircuitBreaker(name = "clienteListarPorIdCB", fallbackMethod = "fallBackcliente")
+    ResponseEntity<ClienteDto> productoBuscarPorId(@PathVariable(required = true) Integer id);
+
+    default ResponseEntity<ClienteDto> fallBackcliente(Integer id) {
+        return ResponseEntity.ok(new ClienteDto());
+
+    }
 }
