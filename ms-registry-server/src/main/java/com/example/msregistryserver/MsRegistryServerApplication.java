5fc0c040d8edba21e9bd99a90c5a43c6de2789b8
eberthrosales <eberthrosalescastillo@gmail.com>
2024-05-27 11:46:51 -0500

    sdfkjhsdkf

--- ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
+++ ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
@@ -6,7 +6,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class MsRegistryServerApplication {
+public class
+
+
+
+
+MsRegistryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsRegistryServerApplication.class, args);
